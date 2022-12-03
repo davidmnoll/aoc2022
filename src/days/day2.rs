cece95d838a2c43955fa7cc4005aa911642666f1
davidmnoll <davidmnoll@users.noreply.github.com>
2022-12-03 00:12:19 -0500

    added tests for day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -98,37 +98,22 @@ mod tests {
 
     #[test]
     fn run1_runs() {
-        let res0 = run1("35", (vec![12, 14], 20));
-        let res1 = run1("", res0);
-        let res2 = run1("3", res1);
-        println!("res2.0 {:?}", res2.0);
-        println!("res2.1 {:?}", res2.1);
-        assert!(res2.1 == 61);
+        let mut res = run1("A X", 0);
+        let res = run1("B Y", res);
+        let res = run1("C Z", res);
+        let res = run1("C Y", res);
+        println!("res2 {:?}", res);
+        assert!(res == 17);
     }
 
 
     #[test]
     fn run2_runs() {
-        let mut res = run2("35", (vec![12, 14], (20, 21, 23)));
-        res = run2("", res);
-        res = run2("31", res);
-        res = run2("32", res);
-        res = run2("33", res);
-        res = run2("", res);
-        res = run2("35", res);
-        res = run2("36", res);
-        res = run2("20", res);
-        res = run2("", res);
-        res = run2("14", res);
-        res = run2("36", res);
-        res = run2("20", res);
-        res = run2("", res);
-        res = run2("54", res);
-        res = run2("56", res);
-        res = run2("50", res);
-        res = run2("", res);
-        println!("res2.0 {:?}", res.0);
-        println!("res2.1 {:?}", res.1);
-        assert!(res.1.0 == 160 && res.1.1 == 96 && res.1.2 == 91);
+        let mut res = run2("A X",0);
+        let res = run2("B Y", res);
+        let res = run2("C Z", res);
+        let res = run2("C Y", res);
+        println!("res2 {:?}", res);
+        assert!(res == 21);
     }
 }
