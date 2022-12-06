976cd426f0ecc4463563035550ac25ae5848c211
davidmnoll <davidmnoll@users.noreply.github.com>
2022-12-06 15:07:24 -0500

    added default run all

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,36 @@ use days::day5;
 use days::day6;
 
 fn main() {
+    let day_match = |x: String|{
+        match x.as_str() {
+            "day1" => day1::DAY.output_results(),
+            "day2" => day2::DAY.output_results(),
+            "day3" => day3::DAY.output_results(),
+            "day4" => day4::DAY.output_results(),
+            "day5" => day5::DAY.output_results(),
+            "day6" => day6::DAY.output_results(),
+            _ => println!("FAILED")
+        }
+    };       
+    let run_all = ||{
+        let now = std::time::Instant::now();
+        for i in 0..6 {
+            day_match(format!("day{}", i + 1))
+        };
+        println!("Total time: {:?}s", now.elapsed().as_secs_f32())
+    };
+ 
     if env::args().len() == 2 {
         let first_argument = env::args().nth(1).unwrap();
         println!("You chose to run: {}", first_argument.clone());
-        let day_match = |x: String|{
-            match x.as_str() {
-                "day1" => day1::DAY.output_results(),
-                "day2" => day2::DAY.output_results(),
-                "day3" => day3::DAY.output_results(),
-                "day4" => day4::DAY.output_results(),
-                "day5" => day5::DAY.output_results(),
-                "day6" => day6::DAY.output_results(),
-                _ => println!("FAILED")
-            }
-        };       
-        if first_argument != "all" {
+       if first_argument != "all" {
             day_match(first_argument.clone());
         }else{
-            let now = std::time::Instant::now();
-            for i in 0..6 {
-                day_match(format!("day{}", i + 1))
-            };
-            println!("Total time: {:?}s", now.elapsed().as_secs_f32())
+            run_all();
         }
     }else if env::args().len() < 2{
-        println!("please enter the day you would like to run e.g. for day 1 enter day1");
+        println!("You chose to run: all");
+        run_all();
     }else{
         println!("too many arguments");
 
